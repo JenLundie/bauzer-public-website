8b4cdbb4b3164465deb21ceab5b5dc94b191e10a
Steve Barnett <steve@naga.co.za>
2019-11-18 16:57:12 +0200

    Add home page intro and outdo to CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("video");
   eleventyConfig.addPassthroughCopy("admin/config.yml");
+
+  eleventyConfig.addFilter("markdownify", function(content) {
+    var MarkdownIt = require("markdown-it");
+    var md = new MarkdownIt();
+    var result = md.render(content);
+    return result;
+  });
 };
